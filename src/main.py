f17fd2dc3678f7e467acc76b272335f9f071238f
Bahry Jarbou <bahry.jarbou@gmail.com>
2025-04-21 21:42:30 +0200

    fixed basepath not referring to the right path

--- src/main.py
+++ src/main.py
@@ -3,11 +3,14 @@ from htmlnode import LeafNode
 from creation_functions import copy_content, generate_pages_recursive
 import sys
 def main():
-    basepath = sys.argv
+    try:
+        basepath = sys.argv[1]
+    except:
+        basepath ="/"
     print(basepath)
     # print("base path:",basepath[0])
     copy_content("./static/","./docs/")
-    generate_pages_recursive("./content/","template.html","./docs/",basepath[0])
+    generate_pages_recursive("./content/","template.html","./docs/",basepath)
     
 
 main()
